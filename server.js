f21ce71f723079bb1c38ca61cd38760138ac90e9
Adam Pawliński <adam.pawlinski@gmail.com>
2018-05-15 00:09:32 +0200

    passport configuration

--- server.js
+++ server.js
@@ -5,8 +5,34 @@ var config = require('./config');
 var app = express();
 var googleProfile = {};
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+  clientID: config.GOOGLE_CLIENT_ID,
+  clientSecret: config.GOOGLE_CLIENT_SECRET,
+  callbackURL: config.CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    googleProfile = {
+      id: profile.id,
+      displayName: profile.displayName
+    };
+    cb(null, profile);
+  }
+));
+
+
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.get('/', function(req, res) {
   res.render('./app_welcome');
